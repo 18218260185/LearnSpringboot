6245ad243c54e0f576e5b8474d5b8f8e14549860
Zengyongquan <568575277@qq.com>
2021-06-29 14:58:05 +0800

    mysql

--- src/main/java/com/how2java/springboot/web/He.java
+++ src/main/java/com/how2java/springboot/web/He.java
@@ -1,12 +1,15 @@
 package com.how2java.springboot.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class He {
     @RequestMapping("/zeng")
     public String zeng(){
+
         return "444449";
     }
 }
